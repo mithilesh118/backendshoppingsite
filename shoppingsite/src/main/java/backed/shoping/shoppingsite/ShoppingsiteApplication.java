fffb7e3fce1de4be7810d0019fc4568b7ac51a42
mithilesh <mithilesh118>
2024-02-18 22:06:54 +0530

    first commit

---
+++ shoppingsite/src/main/java/backed/shoping/shoppingsite/ShoppingsiteApplication.java
@@ -0,0 +1,13 @@
+package backed.shoping.shoppingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingsiteApplication.class, args);
+	}
+
+}
